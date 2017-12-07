790ab5f2312fb1ceb43ecf97664e30bc4893af63
AdrienneBeaudry <adrienne.beaudry@gmail.com>
2017-12-07 08:08:43 +0100

    Curl fetchin  json data from IG API

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,10 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
     <head>
+
+        <?php  //require_once "curl.php";  ?>
+
+
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
